bc80ee60607362e807957e69e8c5209acf2dbd14
pedro <pedro@cs.uni-saarland.de>
2019-05-11 21:53:01 +0200

    add file

---
+++ experiments_main.m
@@ -0,0 +1,7 @@
+function experiments_main
+
+% Calls scripts corresponding to different numerical evaluations of our
+% paper
+
+SBM_case_NegativeInformative           % Figure 3a
+SBM_case_PositiveInformative           % Figure 3b
